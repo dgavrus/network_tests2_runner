97c2d6b6072799c6aa7a484e2a7ecb23b6f97dc7
Alexey Salnikov <alexey.salnikov@gmail.com>
2013-03-25 20:22:55 +0400

    Kommentariy add, permitions on file changed!

--- getnl.sh
+++ getnl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# This line should be uncommented if you run it on supecomputer's interface machine.
+# We verry sorry, but if will not uncomment this line the script will do NOTHING! 
+#
 #sinfo > ~/nodelist
 
 python python/main.py
