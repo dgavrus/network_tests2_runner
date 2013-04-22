c608e86cfaa822a73c3884038e21cc2b2726d829
iksuy <denis.gavrus@gmail.com>
2013-04-22 17:03:49 +0400

    ClusterInfo good modified

--- python/clusterinfo.py
+++ python/clusterinfo.py
@@ -1,14 +1,37 @@
+from partition import Partition
 
 class ClusterInfo:
     
     def __init__(self, sinfo_out):
-        self.__sinfo = sinfo_out
+        self.__sinfo = sinfo_out        
         
-    def get_partition(self, name):
+    def __get_partition_parts(self, name):
         names = self.__get_partition_names()
         if not (name in names) and name != "all":
             return -1
         return self.__parse_sinfo(name)
+        
+    def get_partition_list(self, name):
+        p = self.__get_partition_parts(name)
+        partition = []
+        for current_partition in p:
+            parameters = current_partition.split()
+            partition.append(Partition(parameters[0],parameters[1], \
+                                        parameters[2],parameters[3], \
+                                        parameters[4],parameters[5]))
+        return partition
+        
+    def get_partition_nodes(self, name):
+        all_nodes = []
+        partitions = self.get_partition_list(name)
+        for p in partitions:
+            nodes = p.get_all_nodes()
+            for node in nodes:
+                if all_nodes.count(node) == 0:
+                    all_nodes.append(node)
+        all_nodes.sort(cmp=None, key=None, reverse=False)
+        return all_nodes
+
         
     def __get_partition_names(self):
         partition_names = []
--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ from partition import Partition
 from username import get_username
 from graph import DotGraph
 from clusterinfo import ClusterInfo
+import partition
             
 username = get_username()
 f = open("/home/" + username + "/nodelist")
@@ -16,36 +17,11 @@ for split_string in in_string:
 
 nodelist = f.readlines()
 f.close()
-del(nodelist[0])
+#del(nodelist[0])
 
-cluster_info = ClusterInfo(nodelist)
-partition = cluster_info.get_partition(partition_name)
+cluster_info = ClusterInfo(nodelist[1:])
 
-partitions = []
-if len(partition) > 0:
-    for curr_part in nodelist:
-        if(curr_part.startswith(partition) or partition == "all"):
-            parameters = curr_part.split()
-            partitions.append(Partition(parameters[0],parameters[1], \
-                                        parameters[2],parameters[3], \
-                                        parameters[4],parameters[5]))
-
-num_of_nodes_printed = 0
-num_of_nodes_real = 0
-
-for i in xrange(len(partitions)):
-    print partitions[i].name, partitions[i].state
-    print partitions[i].get_all_nodes()
-
-all_nodes_sorted = []
-
-for p in partitions:
-    nodes = p.get_all_nodes()
-    for node in nodes:
-        if all_nodes_sorted.count(node) == 0:
-            all_nodes_sorted.append(node)
-
-all_nodes_sorted.sort(cmp=None, key=None, reverse=False)
+all_nodes_sorted = cluster_info.get_partition_nodes(partition_name)
 
 nodes_in_partition_file = open("/home/" + username + "/nodes",'w')
 
