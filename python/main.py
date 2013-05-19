256a5e10f845d3e55eb88780798a6e43dee6e2fa
iksuy <denis.gavrus@gmail.com>
2013-05-20 02:10:24 +0400

    Maybe done...

--- python/main.py
+++ python/main.py
@@ -2,27 +2,30 @@ from miniutils import get_username, del_pyc, del_res
 from graph import DotGraph
 from clusterinfo import ClusterInfo
 import subprocess, os
+import time
             
 username = get_username()
 
 del_res()
 
-path = os.getcwd()[:os.getcwd().index(username) + len(username)]
+path = os.getcwd()[:os.getcwd().index(username) + len(username) + 1] + "res/"
 
-#f = open(path + "/nodelist",'w')
-#  
-#subprocess.call(["sinfo"], stdout=f)
-#  
-#f.close()
+f = open(path + "/nodelist",'w')
+ 
+subprocess.call(["sinfo"], stdout=f)
+  
+f.close()
 
-f = open(path + "/nodelist")
+f = open(path[:-4] + "nodelist")
 
 args = raw_input().split()
 
 partition_name = ""
 topology_graph = 0
-number_of_nodes_for_run = 0
+number_of_nodes_for_run = 20
 i = 0
+tasks_per_batch = 4
+
 for i in xrange(len(args)):
         current_arg = args[i]
         if current_arg.startswith("partition="):
@@ -33,7 +36,11 @@ for i in xrange(len(args)):
             number_of_nodes_for_run = int(args[i + 1]) 
         elif current_arg.startswith("--num=") or current_arg.startswith("--number="):
             number_of_nodes_for_run = int(current_arg[str(current_arg).index("=") + 1:])
-        
+        elif current_arg.startswith("--tasks="):
+            tasks_per_batch = int(current_arg[str(current_arg).index("=") + 1:])
+        elif current_arg == "-t":
+            tasks_per_batch = int(args[i + 1])
+                     
 nodelist = f.readlines()
 f.close()
 #del(nodelist[0])
@@ -42,7 +49,7 @@ cluster_info = ClusterInfo(nodelist[1:])
 
 all_nodes_sorted = cluster_info.get_partition_nodes(partition_name)
 
-nodes_in_partition_file = open(path + "/nodes",'w')
+nodes_in_partition_file = open(path + "nodes",'w')
 
 for i in xrange(len(all_nodes_sorted)):
     nodes_in_partition_file.write(all_nodes_sorted[i] + \
@@ -50,17 +57,19 @@ for i in xrange(len(all_nodes_sorted)):
     
 nodes_in_partition_file.close()
 
-nodes_in_partition_file = open(path + "/nodes",'rw')
+nodes_in_partition_file = open(path + "nodes",'rw')
 
 nodes = nodes_in_partition_file.readline().strip().split(",")
 
 i = 0
 offset_i = 0
+nodefile_count = 0
+running_tasks = 0
+test = 0
 
 while i < len(nodes):
     offset_j = offset_i
     j = i + number_of_nodes_for_run / 2
-    nodefile = open(path + "/nodefile",'w')
     m = i + offset_i
     currnodes1 = ""
     while m < i + number_of_nodes_for_run / 2 + offset_i:
@@ -79,8 +88,9 @@ while i < len(nodes):
         if not done:
             offset_i += 1
         m += 1
-    while j +offset_j < len(nodes):
-        nodefile = open(path + "/nodefile",'w')
+    while j + offset_j < len(nodes):
+        nodefile = open(path + "nodefile" + str(nodefile_count + 1) ,'w')
+        nodefile_count += 1
         nodefile.write(currnodes1)
         offset_j = max(offset_j, offset_i)
         k = j + offset_j
@@ -99,17 +109,28 @@ while i < len(nodes):
             k += 1
         nodefile.write('\n')
         nodefile.close()
-        print "sbatch -N8 --nodefile=nodefile ompi network_tests2"
-        f = open(path + "/res/netwtest" + str(i) + str(j),'w')
-        nodefile = open(path + "/nodefile",'r')
-        f.writelines(nodefile.readline())
-        f.close()
-        nodefile.close()
+        if nodefile_count == tasks_per_batch or i + offset_i >= len(nodes) - number_of_nodes_for_run:
+            for nodefile_for_run in os.listdir(path):
+                if nodefile_for_run.startswith("nodefile"):
+                    subprocess.Popen("sbatch -N" + number_of_nodes_for_run + \
+                                             " --nodefile=" + nodefile_for_run + \
+                                             " ompi network_tests2", shell=True)
+                    #print "sbatch -N" + str(number_of_nodes_for_run) + \
+                    #                         " --nodefile=" + nodefile_for_run + \
+                    #                         " ompi network_tests2"
+                    os.remove(path + nodefile_for_run)
+            while nodefile_count == tasks_per_batch:
+                time.sleep(60 * 30)             
+                squeue = subprocess.Popen("squeue | grep " + username, stdout = subprocess.PIPE, shell = True).communicate()[0]
+                tasks_now = squeue.count(username)
+                if tasks_now < tasks_per_batch:
+                    nodefile_count = tasks_now
+                    break
         j += number_of_nodes_for_run / 2
     i += number_of_nodes_for_run / 2
 
 if topology_graph:
-    DotGraph(open(path + "/nodes").readline()).print_graph()
+    DotGraph(open(path + "nodes").readline()).print_graph()
         
 del_pyc()
 
